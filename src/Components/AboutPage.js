0c4c2a58ac3c2157541f77d8bce9998afe0066a6
Rezz88 <zzerep.88@hotmail.com>
2018-05-04 14:46:21 -0400

    Added projects to AboutPage

--- src/Components/AboutPage.js
+++ src/Components/AboutPage.js
@@ -16,16 +16,24 @@ class AboutPage extends Component {
             <div className="container">
                 <h3 className="main-header">React Tutorial</h3>
                 <button onClick={this.renderHome}>Home</button>
-                <hr/>
+                <hr />
                 <h2>My name is Emmanuel Perez,</h2>
                 <p>I'm a junior web developer who recently graduated from DecodeMTL.</p>
                 <h5>MY SKILLS</h5>
                 <ul>
-                    {this.mySkills().map( x => <li>{x}</li>)}
+                    {this.mySkills().map(x => <li>{x}</li>)}
                 </ul>
                 <div>
+                    <h3>Project 1</h3>
+                    <p>This is project 1</p>
+                    <h3>Project 2</h3>
+                    <p>This is project 2</p>
+                    <h3>Project 3</h3>
+                    <p>This is project 3</p>
+                </div>
+                <div>
                     <h4>You can view my resume <a href="https://docs.google.com/document/d/1kUg102acf8BV4rMA1wyFisX9PCcOAwgGgcT0wS9bDTQ/edit?usp=sharing" target="_blank" rel="noopener noreferrer" >HERE</a></h4>
-                    
+
                 </div>
             </div>
         );
